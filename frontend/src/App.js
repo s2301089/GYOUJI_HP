f018ec06dacb6d640e216b390709a935b26045d7
Saku0512 <moonandstar118@gmail.com>
2025-08-29 00:35:05 +0900

    仕事内容のセクションを削除

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Events from './pages/Events';
-import Work from './pages/Work';
+
 import Roles from './pages/Roles';
 import Join from './pages/Join';
 
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Events />} />
-          <Route path="/work" element={<Work />} />
+          
           <Route path="/roles" element={<Roles />} />
           <Route path="/join" element={<Join />} />
         </Routes>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
         <ul>
           <li><NavLink to="/">ホーム</NavLink></li>
           <li><NavLink to="/events">主なイベント</NavLink></li>
-          <li><NavLink to="/work">仕事内容</NavLink></li>
+          
           <li><NavLink to="/roles">役職紹介</NavLink></li>
           <li><NavLink to="/join">参加方法</NavLink></li>
         </ul>
