e9071f52ce94b55be7ceafb4fd1a9ef96c19fd34
Saku0512 <moonandstar118@gmail.com>
2025-09-04 12:52:37 +0900

    reactやめてsvelte

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vue()],
-  css: {
-    postcss: './postcss.config.js'
-  },
-  optimizeDeps: {
-    include: ['tailwindcss']
-  }
-});
+  plugins: [svelte()],
+})
