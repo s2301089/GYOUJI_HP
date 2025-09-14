792084c3810774b07bc0450d90be84077171bcf2
Saku0512 <moonandstar118@gmail.com>
2025-09-15 03:57:52 +0900

    GET /api/scoreのswaggerコメント追加

--- backend/internal/handler/score_handler.go
+++ backend/internal/handler/score_handler.go
@@ -11,6 +11,15 @@ type ScoreHandler struct{ Service service.ScoreServiceInterface }
 
 func NewScoreHandler(s service.ScoreServiceInterface) *ScoreHandler { return &ScoreHandler{Service: s} }
 
+// GetScores 全クラスの出席点を取得
+// @Summary 出席点一覧取得
+// @Description 全クラスの出席点を取得します
+// @Tags score
+// @Produce json
+// @Success 200 {array} model.ScoreBreakdown
+// @Failure 500 {object} map[string]string
+// @Router /api/score [get]
+// @Security ApiKeyAuth
 func (h *ScoreHandler) GetScores(c *gin.Context) {
 	scores, err := h.Service.GetScores()
 	if err != nil {
